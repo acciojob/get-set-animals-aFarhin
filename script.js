48eac47866a7ba632052d8b8e597dfd5c389f538
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-12 14:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ class Animal {
 
 class Cat extends Animal {
   purr() {
-    console.log("purr");
+    console.log("The Siamese makes a sound");
   }
 }
 
 class Dog extends Animal {
   bark() {
-    console.log("woof");
+    console.log("The Golden Retriever makes a sound");
   }
 }
 /*let myCat = new Cat("cat");
